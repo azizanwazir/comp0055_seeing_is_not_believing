85d13dff584406ee085d6616b6ab905842236cb0
Nithin Anand <nithin@hotmail.co.uk>
2021-03-09 19:24:30 +0000

    Implemented basic CLI

--- resizer.py
+++ resizer.py
@@ -4,6 +4,8 @@ import torchvision.transforms.functional as F
 import tensorflow.keras.preprocessing as pre
 import tensorflow as tf
 from PIL import Image
+from termcolor import cprint
+import sys
 
 
 class Resizers:
@@ -41,11 +43,55 @@ class Resizers:
         # but it's what you get back from .shape with cv2's imread
         # i.e cv2.imread(picture).shape --> (height, width)
         atk_img_path = self.folder_path + atk_img_path
-        image = Image.open(atk_img_path)
+        image = cv2.imread(atk_img_path)
         dim = (self.rsz, self.rsz)
         scaled_img = cv2.resize(image, dim)
         self.plot_atk_and_scale(image, scaled_img)
 
 
-Rsz = Resizers("/mnt/WD_Blue_SATAData/Documents/Schoolwork/comp0055/comp0055_seeing_is_not_believing/attack_images/", 229)
-Rsz.tf_resize("atk_jeff.jpg")
+class CLI:
+
+    def __init__(self, folder_path, resize):
+        self.atk_img = None
+        self.Rsz = Resizers(
+            folder_path, resize)
+
+    def selectResizer(self):
+        cprint("Select a resizing method", 'green')
+        cprint("Enter 1 for PyTorch", 'blue')
+        cprint("Enter 2 for TensorFlow", 'blue')
+        cprint("Enter 3 for OpenCV", 'blue')
+        arg = input("Select Option Number for Resizer: ")
+
+        if arg == '1':
+            self.Rsz.pt_resize(self.atk_img)
+        elif arg == '2':
+            self.Rsz.tf_resize(self.atk_img)
+        elif arg == '3':
+            self.Rsz.cv2_resize(self.atk_img)
+        else:
+            cprint("Invalid input, please try again", 'red')
+            self.selectResizer()
+
+    def selectAtkImage(self):
+        cprint("Select an attack image", 'green')
+        cprint("Enter 1 for Cat", 'blue')
+        cprint("Enter 2 for Sheep", 'blue')
+
+        arg = input("Select Option Number for Attack Image: ")
+
+        if arg == '1':
+            self.atk_img = 'atk_cat.jpg'
+        elif arg == '2':
+            self.atk_img = 'atk_sheep.jpg'
+        else:
+            cprint("Invalid input, please try again", 'red')
+            self.atk_img = self.selectAtkImage()
+
+    def startInterface(self):
+        self.selectAtkImage()
+        self.selectResizer()
+
+
+CLI("/mnt/WD_Blue_SATAData/Documents/Schoolwork/comp0055/comp0055_seeing_is_not_believing/attack_images/",
+    229).startInterface()
